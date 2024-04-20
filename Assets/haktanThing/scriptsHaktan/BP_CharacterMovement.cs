f333de8f85d6c0403155c7edaea45e86fc388056
Haktan Çağrı Kodat <134720352+haktan313@users.noreply.github.com>
2024-04-20 04:49:42 +0300

    dash

--- Assets/haktanThing/scriptsHaktan/BP_CharacterMovement.cs
+++ Assets/haktanThing/scriptsHaktan/BP_CharacterMovement.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+
 public class BP_CharacterMovement : MonoBehaviour
 {
     [SerializeField] float playerSpeed;
     [SerializeField] float jumpSpeed;
+    [SerializeField] float dashCooldown;
+    [SerializeField] float dashValue;
 
     bool isGround = false;
+    bool isWalking = false;
+    bool isDashing = false;
     Animator playerAnimator;
     Vector2 playerVector;
     Rigidbody2D playerRB;
@@ -23,17 +29,36 @@ public class BP_CharacterMovement : MonoBehaviour
 
     void Update()
     {
-        
+        if(playerVector.x > 0.01f) 
+        { 
+            transform.localScale = new Vector3(1, 1, 1); 
+        }else if(playerVector.x < -0.01f)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
     }
     void FixedUpdate()
     {
-        playerRB.velocity = new Vector2(playerVector.x * playerSpeed, playerRB.velocity.y);
+        if (!isDashing)
+        {
+            playerRB.velocity = new Vector2(playerVector.x * playerSpeed, playerRB.velocity.y);
+        }
+        if (Mathf.Abs(playerVector.x) > 0.01f)
+        {
+            playerAnimator.SetBool("isWalking", true);
+            isWalking = true;
+        }else
+        {
+            playerAnimator.SetBool("isWalking", false);
+            isWalking = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag ==  "Ground")
         {
+            playerAnimator.SetBool("isJumping", false);
             isGround = true;
         }
     }
@@ -52,6 +77,25 @@ public class BP_CharacterMovement : MonoBehaviour
     void OnJump(InputValue playerInputVector) {
         if(playerInputVector.isPressed == true && isGround) {
             playerRB.velocity += new Vector2(0, jumpSpeed);
+            playerAnimator.SetBool("isJumping",true);
         }
+    }
+    void OnDash(InputValue playerInputVector) {
+        if(playerInputVector.isPressed == true) {
+            Debug.Log("sa");
+            StartCoroutine(Dash());
+        }
+    }
+    IEnumerator Dash() {
+        isDashing = true;
+        float originalGravityScale = playerRB.gravityScale;
+        playerRB.gravityScale = 0;
+        float dashDirection= Mathf.Sign(transform.localScale.x);
+        playerRB.velocity = new Vector2(dashDirection * dashValue, 0);
+
+        yield return new WaitForSeconds(dashCooldown);
+        isDashing=false;
+        playerRB.velocity = Vector2.zero;
+        playerRB.gravityScale = originalGravityScale;
     }
 }
