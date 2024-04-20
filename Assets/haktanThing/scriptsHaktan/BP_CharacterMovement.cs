539b4b886f069e4c18de5cc5e44d8dc4ee871e7a
Haktan Çağrı Kodat <134720352+haktan313@users.noreply.github.com>
2024-04-21 01:23:20 +0300

    attack

--- Assets/haktanThing/scriptsHaktan/BP_CharacterMovement.cs
+++ Assets/haktanThing/scriptsHaktan/BP_CharacterMovement.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+
 public class BP_CharacterMovement : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed;
-    [SerializeField] private float jumpSpeed;
-    [SerializeField] private float dashCooldown;
-    [SerializeField] private float dashValue;
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float dashCooldown;
+    [SerializeField] float dashValue;
 
-    private int comboStep = 0;
-    private float lastAttackTime = 0;
-    private float comboMaxDelay = 1.0f;  // Maximum time between combo hits
-
-    private bool isGround = false;
-    private bool isDashing = false;
-    private Animator playerAnimator;
-    private Vector2 playerVector;
-    private Rigidbody2D playerRB;
-    private CapsuleCollider2D playerCapsuleCollider;
-    private CircleCollider2D playerCircleCollider;
+    bool isGround = false;
+    bool isWalking = false;
+    bool isDashing = false;
+    int comboInt = 0;
+    Animator playerAnimator;
+    Vector2 playerVector;
+    Rigidbody2D playerRB;
+    CapsuleCollider2D playerCapsuleCollider;
+    CircleCollider2D playerCircleCollider;
 
     void Start()
     {
@@ -32,104 +32,90 @@ public class BP_CharacterMovement : MonoBehaviour
 
     void Update()
     {
-        if (playerVector.x > 0.01f)
+        if(playerVector.x > 0.01f) 
+        { 
+            transform.localScale = new Vector3(1, 1, 1); 
+        }else if(playerVector.x < -0.01f)
         {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (playerVector.x < -0.01f)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        // Reset combo if time exceeded
-        if (Time.time - lastAttackTime > comboMaxDelay && comboStep != 0)
-        {
-            comboStep = 0;
-            playerAnimator.ResetTrigger("Attack1");
-            playerAnimator.ResetTrigger("Attack2");
-            playerAnimator.SetTrigger("ResetCombo");
+            transform.localScale = new Vector3(-1,1,1);
         }
     }
-
     void FixedUpdate()
     {
         if (!isDashing)
         {
             playerRB.velocity = new Vector2(playerVector.x * playerSpeed, playerRB.velocity.y);
         }
+        if (Mathf.Abs(playerVector.x) > 0.01f)
+        {
+            playerAnimator.SetBool("isWalking", true);
+            isWalking = true;
+        }else
+        {
+            playerAnimator.SetBool("isWalking", false);
+            isWalking = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Ground")
+        if (other.gameObject.tag ==  "Ground")
         {
             playerAnimator.SetBool("isJumping", false);
             isGround = true;
         }
     }
-
     private void OnCollisionExit2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Ground")
+        if (other.gameObject.tag == "Ground") 
         {
-            isGround = false;
+            isGround = false; 
         }
     }
 
-    void OnMove(InputValue playerInputVector)
-    {
-        playerVector = playerInputVector.Get<Vector2>();
+    void OnMove(InputValue playerInputVector) {
+            playerVector = playerInputVector.Get<Vector2>();
+            Debug.Log("Moving: " + playerVector);
     }
-
-    void OnJump(InputValue playerInputVector)
-    {
-        if (playerInputVector.isPressed && isGround)
-        {
+    void OnJump(InputValue playerInputVector) {
+        if(playerInputVector.isPressed == true && isGround) {
             playerRB.velocity += new Vector2(0, jumpSpeed);
-            playerAnimator.SetBool("isJumping", true);
+            playerAnimator.SetBool("isJumping",true);
         }
     }
-
-    void OnDash(InputValue playerInputVector)
-    {
-        if (playerInputVector.isPressed)
-        {
+    void OnDash(InputValue playerInputVector) {
+        if(playerInputVector.isPressed == true) {
+            Debug.Log("sa");
             StartCoroutine(Dash());
         }
     }
-
-    IEnumerator Dash()
-    {
+    IEnumerator Dash() {
         isDashing = true;
-        playerAnimator.SetBool("dash", true);
+        playerAnimator.Play("Dash");
         float originalGravityScale = playerRB.gravityScale;
         playerRB.gravityScale = 0;
-        float dashDirection = Mathf.Sign(transform.localScale.x);
+        float dashDirection= Mathf.Sign(transform.localScale.x);
         playerRB.velocity = new Vector2(dashDirection * dashValue, 0);
 
         yield return new WaitForSeconds(dashCooldown);
-        isDashing = false;
+        isDashing=false;
         playerRB.velocity = Vector2.zero;
         playerRB.gravityScale = originalGravityScale;
-        playerAnimator.SetBool("dash", false);
-    }
 
+    }
     void OnAttack(InputValue playerInputVector)
     {
-        if (playerInputVector.isPressed)
-        {
-            lastAttackTime = Time.time; // Update the last attack time
+        if (playerInputVector.isPressed == true) {
+            if (comboInt == 0)
+            {
+                playerAnimator.Play("hit1");
+                comboInt = 1;
+            }else if(comboInt == 1)
+            {
+                playerAnimator.Play("Hit2");
+                comboInt = 0;
+            }
 
-            if (comboStep == 0)
-            {
-                playerAnimator.SetTrigger("Attack1");
-                comboStep = 1;
-            }
-            else if (comboStep == 1)
-            {
-                playerAnimator.SetTrigger("Attack2");
-                comboStep = 0;  // Reset combo or set to 2 for a longer combo
-            }
         }
     }
 }
