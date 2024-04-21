3c8f981f758038f9d8d426d58011ef8fe0869635
noviofrivia <68993575+mitsurukuwata@users.noreply.github.com>
2024-04-21 06:16:53 +0300

    hh

--- Assets/Scripts/Enemy Test/EnemyController.cs
+++ Assets/Scripts/Enemy Test/EnemyController.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public Transform player; 
     public float chaseRange = 5f; 
-    public float attackRange = 2f;
+    public float attackRange = 5f; 
     public float moveSpeed = 3f; 
-    public float attackSpeed = 1f;
+    public float attackSpeed = 3f; 
     private bool isAttacking = false; 
 
-    void Update()
+    private Animator animator;
+    private Rigidbody2D rb;
+
+
+    void Start()
     {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {      
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        Debug.Log("DÝSTANCE : " + distanceToPlayer);
 
         if (distanceToPlayer <= attackRange && !isAttacking)
         {
@@ -21,6 +33,7 @@ public class EnemyController : MonoBehaviour
         }
         else if (distanceToPlayer <= chaseRange && !isAttacking)
         {
+            animator.SetFloat("ratSpeed", Mathf.Abs(rb.velocity.x));
             ChasePlayer();
         }
     }
@@ -28,33 +41,20 @@ public class EnemyController : MonoBehaviour
     void ChasePlayer()
     {
         Vector2 targetDirection = (player.position - transform.position).normalized;
-
-        Vector2 newEnemyPosition = (Vector2)transform.position + targetDirection * moveSpeed * Time.deltaTime;
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, targetDirection, moveSpeed * Time.deltaTime);
-        if (hit.collider != null && hit.collider.tag != "Player")
-        {
-            Vector2 hitNormal = hit.normal;
-            targetDirection = Vector2.Reflect(targetDirection, hitNormal);
-            newEnemyPosition = (Vector2)transform.position + targetDirection * moveSpeed * Time.deltaTime;
-        }
-
-        transform.position = newEnemyPosition;
-
-        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        rb.velocity = targetDirection * moveSpeed;
     }
 
     void Attack()
     {
+        animator.SetBool("isAttacking", isAttacking);
         isAttacking = true;
-
-        Debug.Log("Saldýrýyor");
+        Debug.Log("Düþman Saldýrýyor!");
         Invoke("FinishAttack", attackSpeed);
     }
 
     void FinishAttack()
     {
+        animator.SetBool("isAttacking", isAttacking);
         isAttacking = false;
     }
 }
--- Assets/haktanThing/scriptsHaktan/BP_Health.cs
+++ Assets/haktanThing/scriptsHaktan/BP_Health.cs
@@ -6,9 +6,22 @@ public class BP_Health : MonoBehaviour
     [SerializeField] int currentHealth;
     [SerializeField] int maxHealth;
 
+    public float turnSpeed = 180f; 
+    public float fallSpeed = 1f; 
+    private Rigidbody2D rb;
+    private CapsuleCollider2D coll;
+    private Animator animator;
+
     private void Awake()
     {
         currentHealth = maxHealth;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
     public void TakeDamage(int damageAmount)
@@ -18,11 +31,39 @@ public class BP_Health : MonoBehaviour
 
         if (currentHealth <= 0) 
         {
+            animator.enabled = false;
             Dead();
         }
     }
     void Dead()
     {
-       Destroy(gameObject);
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 0f;
+        coll.enabled = false;
+        StartCoroutine(TurnAndFall());
     }
+
+
+    IEnumerator TurnAndFall()
+    {
+        float currentTurnSpeed = 0f;
+
+        while (currentTurnSpeed < turnSpeed)
+        {
+            float turnAmount = turnSpeed * Time.deltaTime;
+            transform.Rotate(0f, 0f, turnAmount);
+            currentTurnSpeed += turnAmount;
+            yield return null;
+        }
+
+        while (transform.position.y > -10f) 
+        {
+            rb.velocity = Vector2.down * fallSpeed;
+            yield return null;
+        }
+
+
+        Destroy(gameObject);
+    }
+
 }
